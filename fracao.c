c774df8ba6a125586d89482a6ca6f8e2abdfd8b1
João Gabriel <jgabriel.ferreira20@gmail.com>
2023-09-22 16:28:48 -0300

    edd(struct) exemplo didatico

--- fracao.c
+++ fracao.c
@@ -5,6 +5,13 @@ struct fracao
 {
     int num, den;
 };
+struct teste
+{
+    int numero;
+    char letra1, letra2;
+    short int pequeno;
+};
+
 int main()
 {
     struct fracao f1, *f2;
@@ -19,6 +26,11 @@ int main()
     scanf("%d", &f2->num);
     printf("\nDigite o denominador de f2: ");
     scanf("%d", &f2->den);
+
+    printf("tamanho da variavel f1: %d\n", sizeof(f1));
+    printf("tamanho da variavel f2: %d\n", sizeof(f2));
+    printf("tamanho do short int: %d\n", sizeof(short int));
+    printf("tamanho da struct teste: %d\n", sizeof(struct teste));
 
     printf("\nf1: %d/%d\n", f1.num, f1.den);
     printf("\nf2: %d/%d\n", f2->num,f2->den);
